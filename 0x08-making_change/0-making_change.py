9b6d9c1b4e7e63933edf7d96c6582bd112927cb9
beloveyebaoh <beloveyeboah@gmail.com>
2024-11-27 22:24:48 +0000

    new

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,14 +7,18 @@ needed to meet a given amount total.
 
 
 def makeChange(coins, total):
+    """ fewest number of coins needed to meet total """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
     for coin in coins:
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
